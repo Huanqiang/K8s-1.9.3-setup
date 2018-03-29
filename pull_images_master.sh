1094fbb7415d5a5b556c213e8b332ca75316da22
Huanqiang <861645007@qq.com>
2018-03-29 16:49:05 +0800

    Update pull_images_master.sh

--- pull_images_master.sh
+++ pull_images_master.sh
@@ -6,10 +6,18 @@ etcd-amd64:3.1.11 \
 pause-amd64:3.0 \
 k8s-dns-sidecar-amd64:1.14.7 \
 k8s-dns-kube-dns-amd64:1.14.7 \
-k8s-dns-dnsmasq-nanny-amd64:1.14.7 \
-kubernetes-dashboard-amd64:v1.8.3)
+k8s-dns-dnsmasq-nanny-amd64:1.14.7)
 for imageName in ${images[@]} ; do
   sudo docker pull mirrorgooglecontainers/$imageName
   sudo docker tag mirrorgooglecontainers/$imageName gcr.io/google_containers/$imageName
   sudo docker rmi mirrorgooglecontainers/$imageName
 done
+
+sudo docker pull jmgao1983/flannel:v0.10.0-amd64
+sudo docker pull mirrorgooglecontainers/kubernetes-dashboard-amd64:v1.8.3
+
+sudo docker tag jmgao1983/flannel:v0.10.0-amd64 quay.io/coreos/flannel:v0.10.0-amd64
+sudo docker tag mirrorgooglecontainers/kubernetes-dashboard-amd64:v1.8.3 k8s.gcr.io/kubernetes-dashboard-amd64:v1.8.3
+
+sudo docker rmi jmgao1983/flannel:v0.10.0-amd64
+sudo docker rmi mirrorgooglecontainers/kubernetes-dashboard-amd64:v1.8.3
