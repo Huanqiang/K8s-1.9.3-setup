0e94e247d317a01526386d986086e76052d7b7bf
Huanqiang <861645007@qq.com>
2018-03-29 16:49:48 +0800

    Update pull_images_worker.sh

--- pull_images_worker.sh
+++ pull_images_worker.sh
@@ -5,7 +5,7 @@ sudo docker pull jmgao1983/flannel:v0.10.0-amd64
 
 sudo docker tag mirrorgooglecontainers/kube-proxy-amd64:v1.9.3 gcr.io/google_containers/kube-proxy-amd64:v1.9.3
 sudo docker tag mirrorgooglecontainers/pause-amd64:3.0 gcr.io/google_containers/pause-amd64:3.0
-sudo docker tag mirrorgooglecontainers/kubernetes-dashboard-amd64:v1.8.3 gcr.io/google_containers/kubernetes-dashboard-amd64:v1.8.3
+sudo docker tag mirrorgooglecontainers/kubernetes-dashboard-amd64:v1.8.3 k8s.gcr.io/kubernetes-dashboard-amd64:v1.8.3
 sudo docker tag jmgao1983/flannel:v0.10.0-amd64 quay.io/coreos/flannel:v0.10.0-amd64
 
 sudo docker rmi mirrorgooglecontainers/kube-proxy-amd64:v1.9.3 
